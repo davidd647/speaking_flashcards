f2dbaf7974e80239d30c19c8fd2e4825499aa176
davidd647 <david.c.dales@gmail.com>
2024-09-08 22:36:34 -0400

    removed non-operational sign from stats screen

--- lib/screens/stats.dart
+++ lib/screens/stats.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:speaking_flashcards/providers/session_logic.dart';
+import 'package:speaking_flashcards/screens/stats/chart.dart';
 
 class Stats extends StatefulWidget {
   const Stats({super.key});
@@ -24,7 +25,7 @@ class _StatsState extends State<Stats> {
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
-          final maxWidth = constraints.maxWidth;
+          // final maxWidth = constraints.maxWidth;
           // final maxHeight = constraints.maxHeight;
 
           return (providerSessionLogic.commonLangs.isEmpty)
@@ -38,19 +39,7 @@ class _StatsState extends State<Stats> {
                   padding: const EdgeInsets.only(left: 10, right: 10),
                   child: Column(
                     children: [
-                      Container(
-                        width: maxWidth,
-                        color: Colors.red,
-                        padding: const EdgeInsets.all(10),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'CURRENTLY NOT OPERATIONAL',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      const StudyChart(),
                       if (providerSessionLogic.batchHistoryList.isNotEmpty) const SizedBox(height: 12),
                       if (providerSessionLogic.batchHistoryList.isNotEmpty)
                         const Padding(
@@ -60,30 +49,30 @@ class _StatsState extends State<Stats> {
                       if (providerSessionLogic.batchHistoryList.isNotEmpty)
                         Container(
                           height: 200,
-                          // color: Colors.grey[100],
                           padding: const EdgeInsets.all(8.0),
                           child: SingleChildScrollView(
-                            // scrollDirection: Axis.horizontal,
                             child: Column(
                               children: [
-                                ...providerSessionLogic.batchHistoryList.map((collection) {
-                                  return Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text('${collection.category},'),
-                                        Flexible(
-                                          child: Text(
-                                            '${collection.name},',
-                                            overflow: TextOverflow.ellipsis,
+                                ...providerSessionLogic.batchHistoryList.map(
+                                  (collection) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(bottom: 8.0),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text('${collection.category},'),
+                                          Flexible(
+                                            child: Text(
+                                              '${collection.name},',
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
                                           ),
-                                        ),
-                                        Text(collection.date.toString()),
-                                      ],
-                                    ),
-                                  );
-                                }),
+                                          Text(collection.date.toString()),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
                               ],
                             ),
                           ),
