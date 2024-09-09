8b14d7d868e9340dbc6640e0098ede1e5ff5fc04
davidd647 <david.c.dales@gmail.com>
2024-09-08 22:35:36 -0400

    chron model now includes lang combo

--- lib/models/chron.dart
+++ lib/models/chron.dart
@@ -1,11 +1,13 @@
 class Chron {
   int id;
   String date;
+  String languageCombo;
   int timeStudied;
 
   Chron({
     required this.id,
     required this.date,
+    required this.languageCombo,
     required this.timeStudied,
   });
 }
