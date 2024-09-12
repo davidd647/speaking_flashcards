5ea52b64c3a1a746eca949b35c45abc81b5bed87
davidd647 <david.c.dales@gmail.com>
2024-09-11 22:09:39 -0400

    dark mode added on settings screen

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -20,9 +20,25 @@ class _SettingsState extends State<Settings> {
     final providerSessionLogic = Provider.of<ProviderSessionLogic>(context);
     final providerSettings = Provider.of<ProviderSettings>(context);
 
+    Color bgColor = Colors.white;
+    // Color containerColor = Colors.grey.shade200;
+    Color fgColor = Colors.black;
+
+    if (providerSettings.darkMode) {
+      bgColor = Colors.black;
+      // containerColor = Colors.grey.shade600;
+      fgColor = Colors.white;
+    }
+
     return Scaffold(
+      backgroundColor: bgColor,
       appBar: AppBar(
-        title: const Text('Settings'),
+        backgroundColor: bgColor,
+        title: Text(
+          'Settings',
+          style: TextStyle(color: fgColor),
+        ),
+        iconTheme: IconThemeData(color: fgColor),
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -42,8 +58,14 @@ class _SettingsState extends State<Settings> {
                     children: [
                       SwitchListTile(
                         activeColor: Colors.blue,
-                        title: const Text('Show Queue'),
-                        subtitle: const Text('Shows all questions in the order they will be asked on the main screen.'),
+                        title: Text(
+                          'Show Queue',
+                          style: TextStyle(color: fgColor),
+                        ),
+                        subtitle: Text(
+                          'Shows all questions in the order they will be asked on the main screen.',
+                          style: TextStyle(color: fgColor),
+                        ),
                         value: providerSettings.showQueue,
                         onChanged: (val) {
                           providerSettings.toggleShowQueue();
@@ -51,12 +73,32 @@ class _SettingsState extends State<Settings> {
                       ),
                       SwitchListTile(
                         activeColor: Colors.blue,
-                        title: const Text('Allow Auto Recog'),
-                        subtitle: const Text(
-                            'When you are correct - after the next question is read, the microphone will start recording the answer immediately.'),
+                        title: Text(
+                          'Allow Auto Recog',
+                          style: TextStyle(color: fgColor),
+                        ),
+                        subtitle: Text(
+                          'When you are correct - after the next question is read, the microphone will start recording the answer immediately.',
+                          style: TextStyle(color: fgColor),
+                        ),
                         value: providerSessionLogic.allowAutoRecog,
                         onChanged: (val) {
                           providerSessionLogic.toggleallowAutoRecog();
+                        },
+                      ),
+                      SwitchListTile(
+                        activeColor: Colors.blue,
+                        title: Text(
+                          'Dark Mode',
+                          style: TextStyle(color: fgColor),
+                        ),
+                        subtitle: Text(
+                          'A display mode better suited for studying in dark environments.',
+                          style: TextStyle(color: fgColor),
+                        ),
+                        value: providerSettings.darkMode,
+                        onChanged: (val) {
+                          providerSettings.toggledarkMode();
                         },
                       ),
                       // const SizedBox(height: 24),
