dc23b4d676dc79d07a42d0121e45e58075b590ae
davidd647 <david.c.dales@gmail.com>
2024-09-13 11:06:14 -0400

    wide button now has color (not primary/disabled)

--- lib/widgets/wide_button.dart
+++ lib/widgets/wide_button.dart
@@ -5,23 +5,26 @@ class WideButton extends StatelessWidget {
     super.key,
     required this.onTap,
     required this.child,
-    this.primary = false,
-    this.disabled = false,
+    required this.color,
+    // this.primary = false,
+    // this.disabled = false,
   });
 
   final Function onTap;
   final Widget child;
-  final bool primary;
-  final bool disabled;
+  final Color color;
+  // final bool primary;
+  // final bool disabled;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: disabled
-          ? Colors.grey[200]
-          : primary
-              ? Colors.lightBlue[100]
-              : Colors.grey[300],
+      color: color,
+      // color: disabled
+      //     ? Colors.grey[200]
+      //     : primary
+      //         ? Colors.lightBlue[100]
+      //         : Colors.grey[300],
       child: InkWell(
         onTap: () => onTap(),
         child: Container(
