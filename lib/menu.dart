5a50f7a82705fa3f1a463929e9abf6fe1fdb5b3e
davidd647 <david.c.dales@gmail.com>
2024-10-04 12:14:42 -0400

    semver update

--- lib/menu.dart
+++ lib/menu.dart
@@ -547,7 +547,7 @@ class _MenuContainerState extends State<MenuContainer> {
               const SizedBox(height: 50),
               const Center(
                 child: Text(
-                  'version: 1.0.28+29',
+                  'version: 1.0.29+30',
                   // make it white because the drawer is always a black or dark shade
                   style: TextStyle(color: Colors.white),
                 ),
