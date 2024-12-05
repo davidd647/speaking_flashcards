9ce2f104b446ffba0cfa022d4ad0dc0c12976763
davidd647 <david.c.dales@gmail.com>
2024-12-04 22:00:11 -0500

    arcs and checks instead of esoteric ratios (unless debuggin)

--- lib/widgets/queue_descending.dart
+++ lib/widgets/queue_descending.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:math';
 
 import 'package:speaking_flashcards/providers/session_logic.dart';
 import 'package:speaking_flashcards/providers/settings.dart';
@@ -55,7 +56,7 @@ class QuestionQueueDescending extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text('${question.order + 1} ', style: TextStyle(color: textColor)),
-                  if (question.level >= 3) Icon(Icons.check, color: textColor, size: 12),
+                  // if (question.level >= 3) Icon(Icons.check, color: textColor, size: 12),
                   Flexible(
                     fit: FlexFit.tight,
                     child: Padding(
@@ -70,13 +71,38 @@ class QuestionQueueDescending extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Text(
-                    '${question.level}/${question.spiritLevel}',
-                    style: TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      color: textColor,
+                  if (!providerSettings.debugMode && question.level < 3)
+                    Container(
+                      width: 18,
+                      height: 18,
+                      padding: const EdgeInsets.all(2),
+                      child: WedgeWidget(percentage: question.level * 0.33, color: textColor),
                     ),
-                  ),
+                  if (question.level >= 3)
+                    SizedBox(
+                      width: 18,
+                      height: 18,
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: Icon(
+                              Icons.check_circle,
+                              color: Colors.lightBlue[200],
+                              size: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // child: Text('👍'),
+                    ),
+                  if (providerSettings.debugMode)
+                    Text(
+                      '${question.level}/${question.spiritLevel}',
+                      style: TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        color: textColor,
+                      ),
+                    ),
                 ],
               ),
             ),
@@ -84,6 +110,54 @@ class QuestionQueueDescending extends StatelessWidget {
           );
         }),
       ],
+    );
+  }
+}
+
+class WedgePainter extends CustomPainter {
+  final double percentage; // The percentage of the circle to draw
+  final Color textColor;
+
+  WedgePainter({required this.percentage, required this.textColor});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = textColor
+      ..style = PaintingStyle.fill;
+
+    // Define the rectangle for the circle
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    // Draw the arc (wedge)
+    canvas.drawArc(
+      rect,
+      -pi / 2, // Start angle (top center)
+      2 * pi * percentage, // Sweep angle based on percentage
+      true, // Use center for wedge shape
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class WedgeWidget extends StatelessWidget {
+  final double percentage; // The percentage of the circle to draw
+  final Color color;
+
+  WedgeWidget({required this.percentage, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomPaint(
+        size: const Size(200, 200), // Specify the size of the circle
+        painter: WedgePainter(percentage: percentage, textColor: color),
+      ),
     );
   }
 }
